07e1315c44b8c455ae5f1ee5c7e4180fbde43ec2
Riyat121 <145189781+Riyat121@users.noreply.github.com>
2026-03-30 20:57:27 +0530

    cors error

--- backend/server.js
+++ backend/server.js
@@ -16,21 +16,27 @@ const PORT = process.env.PORT || 5000;
 
 
 
+
 app.use(cors({
   origin: [
     "http://localhost:5173",
-    "https://prep-genius-9ralkorjh-riyat121s-projects.vercel.app"
+    "https://prep-genius-ashen.vercel.app"
   ],
   methods: ["GET", "POST", "PUT", "DELETE"],
   credentials: true
-}))
+}));
 
 // ✅ JSON parser
 app.use(express.json());
 
 // connect DB
 connectDB();
-
+app.options("*", cors());
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "https://prep-genius-ashen.vercel.app");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+  next();
+});
 // routes
 app.use("/api/auth", authRoutes);
 app.use("/api/interview", interviewRoutes);
